e1ce4fea5231ec23c2fa3c071bcbd5a978f37003
Pedro Silva <pedrosi@microsoft.com>
2022-08-18 15:14:17 -0700

    Added end to end tests to verify that config is correct and command-line parameters work.

--- tests/D20Tek.Tools.CreateGuid.Tests/Common/CommandAppTestContext.cs
+++ tests/D20Tek.Tools.CreateGuid.Tests/Common/CommandAppTestContext.cs
@@ -3,6 +3,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 using D20Tek.Spectre.Console.Extensions.Injection;
 using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace D20Tek.Tools.CreateGuid.Tests.Common
@@ -19,6 +20,42 @@ namespace D20Tek.Tools.CreateGuid.Tests.Common
         {
             Registrar = new DependencyInjectionTypeRegistrar(new ServiceCollection());
             Configurator = new FakeConfigurator(Registrar);
+        }
+
+        public async Task<CommandAppBasicResult> RunAsync(
+            Func<string[], Task<int>> mainEntryPointAsync,
+            string commandLine)
+        {
+            var args = commandLine.Split(' ', '\t');
+            return await RunAsync(mainEntryPointAsync, args);
+        }
+
+        public async Task<CommandAppBasicResult> RunAsync(
+            Func<string[], Task<int>> mainEntryPointAsync,
+            string[] args)
+        {
+            ArgumentNullException.ThrowIfNull(mainEntryPointAsync, nameof(mainEntryPointAsync));
+
+            AnsiConsole.Record();
+            var exitCode = await mainEntryPointAsync(args);
+
+            return new CommandAppBasicResult(exitCode, AnsiConsole.ExportText());
+        }
+
+        public CommandAppBasicResult Run(Func<string[], int> mainEntryPoint, string commandLine)
+        {
+            var args = commandLine.Split(' ', '\t');
+            return Run(mainEntryPoint, args);
+        }
+
+        public CommandAppBasicResult Run(Func<string[], int> mainEntryPoint, string[] args)
+        {
+            ArgumentNullException.ThrowIfNull(mainEntryPoint, nameof(mainEntryPoint));
+
+            AnsiConsole.Record();
+            var exitCode = mainEntryPoint(args);
+
+            return new CommandAppBasicResult(exitCode, AnsiConsole.ExportText());
         }
     }
 }
--- tests/D20Tek.Tools.CreateGuid.Tests/ProgramE2ETests.cs
+++ tests/D20Tek.Tools.CreateGuid.Tests/ProgramE2ETests.cs
@@ -1,6 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 // Copyright (c) d20Tek.  All rights reserved.
 //---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Tools.CreateGuid.Tests.Common;
 
 namespace D20Tek.Tools.CreateGuid.Tests
 {
@@ -8,8 +9,52 @@ namespace D20Tek.Tools.CreateGuid.Tests
     public class ProgramE2ETests
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task CreateGuidOperation_Default()
         {
+            // arrange
+            var context = new CommandAppTestContext();
+
+            // act
+            var result = await context.RunAsync(Program.Main, Array.Empty<string>());
+
+            // assert
+            Assert.AreEqual(0, result.ExitCode);
+            StringAssert.Contains(result.Output, "create-guid");
+            StringAssert.Contains(result.Output, "completed successfully");
+        }
+
+        [TestMethod]
+        public async Task CreateGuidOperation_EmptyGuid()
+        {
+            // arrange
+            var context = new CommandAppTestContext();
+            var args = "-f Default -e -u";
+
+            // act
+            var result = await context.RunAsync(Program.Main, args);
+
+            // assert
+            Assert.AreEqual(0, result.ExitCode);
+            StringAssert.Contains(result.Output, "create-guid");
+            StringAssert.Contains(result.Output, "00000000-0000-0000-0000-000000000000");
+            StringAssert.Contains(result.Output, "completed successfully");
+        }
+
+
+        [TestMethod]
+        public async Task CreateGuidOperation_MultipleGuids()
+        {
+            // arrange
+            var context = new CommandAppTestContext();
+            var args = "--count 5";
+
+            // act
+            var result = await context.RunAsync(Program.Main, args);
+
+            // assert
+            Assert.AreEqual(0, result.ExitCode);
+            StringAssert.Contains(result.Output, "create-guid");
+            StringAssert.Contains(result.Output, "completed successfully");
         }
     }
 }
