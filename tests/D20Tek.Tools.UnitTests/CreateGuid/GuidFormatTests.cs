600c46cacfdec9d500e4f766c43b01d41ffa6162
Pedro Silva <DarthPedro@gmail.com>
2025-05-30 17:22:37 -0700

    Added unit tests for various GuidFormat mappings.

---
+++ tests/D20Tek.Tools.UnitTests/CreateGuid/GuidFormatTests.cs
@@ -0,0 +1,29 @@
+﻿using D20Tek.Tools.CreateGuid;
+
+namespace D20Tek.Tools.UnitTests.CreateGuid;
+
+[TestClass]
+public class GuidFormatTests
+{
+    [TestMethod]
+    [DataRow(GuidFormat.D, "D")]
+    [DataRow(GuidFormat.Default, "D")]
+    [DataRow(GuidFormat.N, "N")]
+    [DataRow(GuidFormat.Number, "N")]
+    [DataRow(GuidFormat.B, "B")]
+    [DataRow(GuidFormat.Braces, "B")]
+    [DataRow(GuidFormat.P, "P")]
+    [DataRow(GuidFormat.Parens, "P")]
+    [DataRow(GuidFormat.X, "X")]
+    [DataRow(GuidFormat.Hex, "X")]
+    public void ToFormatString_WithGuidFormat_MapsToExpectedString(GuidFormat format, string expected)
+    {
+        // arrange
+
+        // act
+        var result = format.ToFormatString();
+
+        // assert
+        Assert.AreEqual(expected, result);
+    }
+}
