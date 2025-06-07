d8e7593c41745d6e8a2824637af0c0446a9d1bcd
Pedro Silva <DarthPedro@gmail.com>
2025-06-06 23:45:13 -0700

    Created skeletal CLI project for NuGet.Portfolio with no initial commands. Changed assembly name to nu-port. Added reference to D20Tek.Spectre.Console.Extensions.

---
+++ src/D20Tek.NuGet.Portfolio/Program.cs
@@ -0,0 +1,13 @@
+﻿using D20Tek.Spectre.Console.Extensions;
+
+namespace D20Tek.NuGet.Portfolio;
+
+public sealed class Program
+{
+    public static async Task<int> Main(string[] args) =>
+        await new CommandAppBuilder().WithDIContainer()
+                                     .WithStartup<Startup>()
+                                     //.WithDefaultCommand<CreateGuidCommand>()
+                                     .Build()
+                                     .RunAsync(args);
+}
---
+++ src/D20Tek.NuGet.Portfolio/Startup.cs
@@ -0,0 +1,16 @@
+﻿using D20Tek.Spectre.Console.Extensions;
+using Spectre.Console.Cli;
+
+namespace D20Tek.NuGet.Portfolio;
+
+internal sealed class Startup : StartupBase
+{
+    public override IConfigurator ConfigureCommands(IConfigurator config)
+    {
+        return config;
+    }
+
+    public override void ConfigureServices(ITypeRegistrar registrar)
+    {
+    }
+}
