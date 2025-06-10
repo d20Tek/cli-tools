a1f2b80280025191ed86aae45bc59ea3022ae5b9
Pedro Silva <DarthPedro@gmail.com>
2025-06-10 00:36:08 -0700

    Add sync version of ResultPresenter.Render method.

--- src/D20Tek.NuGet.Portfolio/Common/Controls/ResultPresenter.cs
+++ src/D20Tek.NuGet.Portfolio/Common/Controls/ResultPresenter.cs
@@ -8,6 +8,12 @@ internal static class ResultPresenter
             s => Task.FromResult(RenderSuccess(console, successMessage(s))),
             e => Task.FromResult(RenderErrors(console, e)));
 
+    public static int Render<T>(this Result<T> result, IAnsiConsole console, Func<T, string> successMessage)
+        where T : notnull =>
+        result.Match(
+            s => RenderSuccess(console, successMessage(s)),
+            e => RenderErrors(console, e));
+
     private static int RenderSuccess(IAnsiConsole console, string message)
     {
         console.MarkupLine($"[green]Success:[/] {Markup.Escape(message)}");
@@ -19,24 +25,19 @@ internal static class ResultPresenter
         if (errors.Count() > 1)
         {
             console.MarkupLine("[red]Multiple error messages[/]:");
-            foreach (var error in errors)
-            {
-                var msg = Markup.Escape(error.Message);
-                console.MarkupLine($" - {msg}");
-            }
+            errors.ForEach(e => console.MarkupLine($" - {Markup.Escape(e.Message)}"));
         }
         else if (errors.Count() == 1)
         {
-            RenderError(errors.First(), console);
+            RenderError(console, errors.First());
         }
 
         return Globals.E_FAIL;
     }
 
-    private static int RenderError(Error error, IAnsiConsole console)
+    private static int RenderError(IAnsiConsole console, Error error)
     {
-        var msg = Markup.Escape(error.Message);
-        console.MarkupLine($"[red]Error[/]: {msg}");
+        console.MarkupLine($"[red]Error[/]: {Markup.Escape(error.Message)}");
         return Globals.E_FAIL;
     }
 }
