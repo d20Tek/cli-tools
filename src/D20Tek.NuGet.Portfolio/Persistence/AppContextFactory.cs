0bffd181dc5a5c8072d50f5b4ded2e87f6fdcb70
Pedro Silva <DarthPedro@gmail.com>
2025-06-10 17:05:41 -0700

    Added unit tests for AppDbContext.ApplyMigrations function.

--- src/D20Tek.NuGet.Portfolio/Persistence/AppContextFactory.cs
+++ src/D20Tek.NuGet.Portfolio/Persistence/AppContextFactory.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics.CodeAnalysis;
 
 namespace D20Tek.NuGet.Portfolio.Persistence;
 
 internal sealed class AppContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
+    private readonly string _connection;
+
+    [ExcludeFromCodeCoverage]
+    public AppContextFactory(string? connection = null)
+    {
+        _connection = connection ?? "Data Source=nu-port.db";
+    }
+
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlite("Data Source=nu-port.db");
+        optionsBuilder.UseSqlite(_connection);
 
-        return new AppDbContext(optionsBuilder.Options, null!);
+        return new AppDbContext(optionsBuilder.Options, new NullLogger<AppDbContext>());
     }
 }
---
+++ tests/D20Tek.Tools.UnitTests/NuGetPortfolio/Persistence/AppDbContextTests.cs
@@ -0,0 +1,35 @@
+﻿using D20Tek.NuGet.Portfolio.Persistence;
+using D20Tek.Tools.UnitTests.NuGetPortfolio.Fakes;
+using Microsoft.Data.Sqlite;
+
+namespace D20Tek.Tools.UnitTests.NuGetPortfolio.Persistence;
+
+[TestClass]
+public class AppDbContextTests
+{
+
+    [TestMethod]
+    public void ApplyMigrations_WithTempDatabaseFile_ShouldRunMigrations()
+    {
+        // arrange
+        string conn = $"Data Source={Guid.NewGuid()}.db";
+        var dbContext = new AppContextFactory(conn).CreateDbContext([]);
+
+        // act
+        dbContext.ApplyMigrations();
+
+        // assert
+    }
+
+
+    [TestMethod]
+    public void ApplyMigrations_WithMemoryDb_ShouldThrowException()
+    {
+        // arrange
+        var dbContext = InMemoryDbContext.Create();
+
+        // act - assert
+        Assert.Throws<SqliteException>(dbContext.ApplyMigrations);
+    }
+
+}
