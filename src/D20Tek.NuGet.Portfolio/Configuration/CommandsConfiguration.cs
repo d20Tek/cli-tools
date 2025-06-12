1de987a667e5e1be51a605893a6ed54a0a05eabe
Pedro Silva <DarthPedro@gmail.com>
2025-06-12 00:12:16 -0700

    Added more aliases to collection commands configuration.

--- src/D20Tek.NuGet.Portfolio/Configuration/CommandsConfiguration.cs
+++ src/D20Tek.NuGet.Portfolio/Configuration/CommandsConfiguration.cs
@@ -24,23 +24,30 @@ internal static class CommandsConfiguration
         config.AddBranch("collection", bc =>
                 {
                     bc.AddCommand<ListCollectionsCommand>("list")
+                      .WithAlias("ls")
                       .WithDescription("Lists all of your package collections.")
                       .WithExample(["collection", "list"]);
 
                     bc.AddCommand<AddCollectionCommand>("add")
+                      .WithAlias("a")
+                      .WithAlias("+")
                       .WithDescription("Adds a new package collection that can be used for organization and tracking.")
                       .WithExample(["collection", "add", "--name", "New Collection"]);
 
                     bc.AddCommand<EditCollectionCommand>("edit")
+                      .WithAlias("ed")
+                      .WithAlias("e")
                       .WithDescription("Edits an existing package collection.")
                       .WithExample(["collection", "edit", "--id", "123", "--name", "New Collection"]);
 
                     bc.AddCommand<DeleteCollectionCommand>("delete")
                       .WithAlias("del")
+                      .WithAlias("d")
                       .WithDescription("Deletes a package collection by its id.")
                       .WithExample(["collection", "delete", "--id", "123"]);
                 })
-              .WithAlias("coll");
+              .WithAlias("coll")
+              .WithAlias("c");
 
         return config;
     }
